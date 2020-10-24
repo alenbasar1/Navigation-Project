4ed846bbaeca38fbf534024237c73a5a64132b55
alen <alenbasar@outlook.com>
2020-10-25 01:45:29 +1100

    done for tonight

---
+++ src/components/SidebarData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Projects',
+    path: '/projects',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'About',
+    path: '/about',
+    icon: <FaIcons.FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+    icon: <IoIcons.IoMdPeople />,
+    cName: 'nav-text'
+  }
+];
---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className='products'>
+      <h1>Project Page</h1>
+    </div>
+  );
+}
+
+export default Projects;
